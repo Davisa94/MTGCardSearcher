2fcc397ee99a5e7fc368d0e0c27bfb5d37310243
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-10 16:07:01 -0700

    added descriptions for each model

--- Backend/MTGDeck_manager/api/models.py
+++ Backend/MTGDeck_manager/api/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+##################################################
+# Holds all information needed to display the card
+# for the user
+# Author: Austin Benitez
+##################################################
 class Card(models.Model):
     multiverseid = models.IntegerField(unique=True) # Used to fetch the card from the API
     name = models.CharField(max_length=255)
 
+##################################################
+# Holds all information needed to display a users 
+# curated list of cards and related information
+# Author: Austin Benitez
+##################################################
 class Deck(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
+
