2878ece692ec825ac0a5d5f86be4cce65ac9e5d0
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-10 16:48:26 -0700

    made name field longer than the longest cardname: "Our Market Research Shows That Players Like Really Long Card Names So We Made this Card to Have the Absolute Longest Card Name Ever Elemental"

--- Backend/MTGDeck_manager/api/models.py
+++ Backend/MTGDeck_manager/api/models.py
@@ -9,17 +9,17 @@ from django.db import models
 ##################################################
 class Card(models.Model):
     multiverseid = models.IntegerField(unique=True) # Used to fetch the card from the API
-    # deck_id = models.ForeignKey(Deck.id)
+    deck_id = models.ForeignKey("Deck", related_name="card", on_delete=models.CASCADE)
 
 ##################################################
 # An info table containing everything about the
-# Card given from 
+# Card given from the API used as a cache
 # Author: Austin Benitez
 ##################################################
-class CardListItem(models.Model):
-
+class CardInfo(models.Model):
+    id = models.AutoField(primary_key=True)
     multiverseid = models.IntegerField(unique=True)
-    # quantity
+    name = models.CharField(max_length=145)
 
 ##################################################
 # Holds all information needed to display a users 
@@ -28,5 +28,5 @@ class CardListItem(models.Model):
 ##################################################
 class Deck(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=145)
 
