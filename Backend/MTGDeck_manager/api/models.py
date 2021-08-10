43a8acf763975712e80629156449bfe243977d19
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-10 13:26:04 -0700

    Simplifying the card for v1 to have multiverseid to fetch card info when needed

--- Backend/MTGDeck_manager/api/models.py
+++ Backend/MTGDeck_manager/api/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Card(models.Model):
-    multiverseid = models.IntegerField(unique=True)
+    multiverseid = models.IntegerField(unique=True) # Used to fetch the card from the API
+    name = models.CharField(max_length=255)
 
 class Deck(models.Model):
     id = models.AutoField(primary_key=True)
