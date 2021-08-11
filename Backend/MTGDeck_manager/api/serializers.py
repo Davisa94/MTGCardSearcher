52ba8019256ea2543fb2165d099a356889a6024a
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-10 19:23:53 -0700

    more serializer work

--- Backend/MTGDeck_manager/api/serializers.py
+++ Backend/MTGDeck_manager/api/serializers.py
@@ -2,4 +2,19 @@ from .models import *
 from rest_framework import serializers
 
 
+class CardSerializer(serializers):
+    class Meta:
+        model = Card
+        fields = (__all__)
+
+class CardToDeckSerializer(serializers):
+    class Meta:
+        model = CardToDeck
+        fields = ('deck_id', 'card_id')
+
 # Serializer for Deck that grabs the nested model references
+class DeckSerializer(serializers):
+    cards = serializers.PrimaryKeyRelatedField(many=True)
+    class Meta:
+        model = Deck
+        fields = (__all__, 'cards')
