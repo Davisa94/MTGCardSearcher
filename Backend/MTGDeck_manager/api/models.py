dc00bf3d46e118afe4c75eef844895e3d14c53f6
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-10 17:19:16 -0700

    further cleaning up the Relational model

--- Backend/MTGDeck_manager/api/models.py
+++ Backend/MTGDeck_manager/api/models.py
@@ -7,17 +7,17 @@ from django.db import models
 # Author: Austin Benitez
 ##################################################
 class CardToDeck(models.Model):
-    multiverseid = models.IntegerField(unique=True) # Used to fetch the card from the API
-    deck_id = models.ForeignKey("Deck", related_name="card", on_delete=models.CASCADE)
+    deck_id = models.ForeignKey("Deck", related_name="card_to_deck", on_delete=models.CASCADE)
+    card_id = models.ForeignKey("Card", related_name="card_to_deck", on_delete=models.CASCADE)
 
 ##################################################
 # An info table containing everything about the
 # Card given from the API used as a cache
 # Author: Austin Benitez
 ##################################################
-class CardInfo(models.Model):
+class Card(models.Model):
     id = models.AutoField(primary_key=True)
-    multiverseid = models.IntegerField(unique=True)
+    multiverseid = models.IntegerField(unique=True) # Used to fetch the card from the API
     name = models.CharField(max_length=145)
 
 ##################################################
