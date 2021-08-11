5cc4ecc6a264ceeb2ccb7b042f3cefc64fd29bdf
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-10 21:22:03 -0700

    Adding more fields to the serializer

--- Backend/MTGDeck_manager/api/serializers.py
+++ Backend/MTGDeck_manager/api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
-        fields = ('id')
+        fields = ('id', 'name')
 
 class CardToDeckSerializer(serializers.ModelSerializer):
     class Meta:
