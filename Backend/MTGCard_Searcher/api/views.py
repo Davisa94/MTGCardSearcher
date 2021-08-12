8323c421ade15169ff876fc9d3cd85fe698ce527
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-11 20:48:15 -0700

    Changing project scope to deliver on time

--- Backend/MTGCard_Searcher/api/views.py
+++ Backend/MTGCard_Searcher/api/views.py
@@ -13,3 +13,6 @@ def main(req):
 class DeckView(generics.CreateAPIView):
     queryset = Deck.objects.all()
     serializer_class = DeckSerializer
+class CardView(generics.creatAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
