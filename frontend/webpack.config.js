bae8f5c5b5b587c50e057870aacd9c4816908f49
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-14 21:41:37 -0700

    fixed an issue with the webpack settings

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/main.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
     filename: "[name].js",
