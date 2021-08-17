423c31db7efc5eca01a87aa7a0fdf9d192f51b1d
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-16 18:32:17 -0700

    removed unused code

--- Backend/MTGCard_Searcher/api/serializers.py
+++ Backend/MTGCard_Searcher/api/serializers.py
@@ -1,4 +1,4 @@
-from .models import *
+from .models import Card
 from rest_framework import serializers
 
 
@@ -7,25 +7,25 @@ class CardSerializer(serializers.ModelSerializer):
         model = Card
         fields = ('id', 'name', 'nickname', 'multiverseid', 'imageURL')
 
-class CardToDeckSerializer(serializers.ModelSerializer):
-    card_info = CardSerializer()
-    class Meta:
-        model = CardToDeck
-        fields = ('deck_id', 'card_id', 'card_info')
-        depth = 1
+# class CardToDeckSerializer(serializers.ModelSerializer):
+#     card_info = CardSerializer()
+#     class Meta:
+#         model = CardToDeck
+#         fields = ('deck_id', 'card_id', 'card_info')
+#         depth = 1
 
-# Serializer for Deck that grabs the nested model references
-class DeckSerializer(serializers.ModelSerializer):
-    # if not intended to use bulk action it's better to keep may fields as read only
-    cards = CardToDeckSerializer(many=True)
-    class Meta:
-        model = Deck
-        depth = 0
-        fields = ('name', 'cards')
-    def create(self, validated_data):
-        print("THE DATA RECIEVED WAS: ", validated_data)
-        cards_data = validated_data.pop('cards')
-        deck = Deck.objects.create(**validated_data)
-        for card in cards_data:
-            Card.objects.create(id=id, name=name)
-        return deck
+# # Serializer for Deck that grabs the nested model references
+# class DeckSerializer(serializers.ModelSerializer):
+#     # if not intended to use bulk action it's better to keep may fields as read only
+#     cards = CardToDeckSerializer(many=True)
+#     class Meta:
+#         model = Deck
+#         depth = 0
+#         fields = ('name', 'cards')
+#     def create(self, validated_data):
+#         print("THE DATA RECIEVED WAS: ", validated_data)
+#         cards_data = validated_data.pop('cards')
+#         deck = Deck.objects.create(**validated_data)
+#         for card in cards_data:
+#             Card.objects.create(id=id, name=name)
+#         return deck
