bd3e78cf413a4f81613d3c7ce6f9ba09ede2888f
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-17 14:58:26 -0700

    added final two components as skeletons

---
+++ Backend/MTGCard_Searcher/frontend/src/components/CardSearch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CardSearch extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <h2>Card Search</h2>
+    }
+}
---
+++ Backend/MTGCard_Searcher/frontend/src/components/NickNameCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NickNameCard extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <h2>Nick Name a Card</h2>
+    }
+}
